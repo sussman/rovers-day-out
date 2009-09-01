a97d9bfb5976a9dea17069ab906909b97fc1e2cb
Ben Collins-Sussman <sussman@red-bean.com>
2009-09-01 17:43:01 -0500

    minor tweaks

---
+++ full_pull.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script assumes you're sitting in your 'rover' working copy, and
+# that 'rover.inform' is at the top of the working copy.
+
+# Make sure Inform is *not* running when you run this script!
+
+echo "Pulling..."
+hg pull https://rovers-day-out.googlecode.com/hg
+echo "Updating..."
+hg update
+echo "Copying into rover.inform/ ..."
+cp story.ni rover.inform/Source/story.ni
+echo "Done."
+
