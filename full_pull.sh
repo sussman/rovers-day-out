f7a6153055d05b2f7b09e3d644b713e84090d715
Ben Collins-Sussman <sussman@red-bean.com>
2009-09-02 22:32:14 -0500

    Fix issue 24: Instead of searching the picture (l picture), examine it.

---
+++ full_pull.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script assumes you're sitting in your 'rover' working copy, and
+# that 'rover.inform' is at the top of the working copy.
+
+# Make sure Inform is *not* running when you run this script!
+
+echo "Pulling..."
+hg pull rover
+echo "Updating..."
+hg update
+echo "Copying into rover.inform/ ..."
+cp story.ni rover.inform/Source/story.ni
+echo "Done."
+
